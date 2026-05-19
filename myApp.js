825522a1cfca4c5447a5063b4252b982b20017f1
test-user <157971136+apilogtest@users.noreply.github.com>
2026-05-19 19:07:17 -0300

    feat(Exerc. 10): get query parameter input

--- myApp.js
+++ myApp.js
@@ -63,5 +63,11 @@ app.get('/:word/echo', (req, res) =>{
     res.json({ echo: word });
 });
 
+// Exerc. 10
+app.get('/name', (req, res) => {
+
+    res.json({ name: req.query.first + " " + req.query.last });
+});
+
 
  module.exports = app;
